39238cfe0bf5067c1993a871046e8453ef1d96dc
huboqiang1 <54888051+huboqiang1@users.noreply.github.com>
2022-10-14 10:15:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday_man, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
-  return (next.day - today.day).days
+  return (next - today).days
 
 def get_birthday_man():
 #   next = datetime.strptime(str(date.today().year) + "-" + birthday_man, "%Y-%m-%d")
