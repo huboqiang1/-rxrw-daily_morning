744ebe3f73acbb2519780f56ac7331fee4ae8b75
huboqiang1 <54888051+huboqiang1@users.noreply.github.com>
2023-02-17 10:51:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def get_weather():
   print(type(res))
   print(res)
   print(res['data'])
-  print(res['data'['tq']])
+  print(res['data']['tq'])
   print(res['data']['qw']))
   weather = res['data']['tq']
   return weather['weather'], int(res['data']['qw'])
