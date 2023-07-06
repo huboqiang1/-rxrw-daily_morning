71d33d8b9b92dcffec839a85aa94d445c09fd254
huboqiang1 <54888051+huboqiang1@users.noreply.github.com>
2023-07-06 14:53:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -103,9 +103,8 @@ def get_birthday_man():
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+  if words.data != null:
+    return words.json()['data']['text']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
