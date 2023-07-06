482ecc1bb2d6746a09d628ac5c37807926fa1e6c
huboqiang1 <54888051+huboqiang1@users.noreply.github.com>
2023-07-06 15:02:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def get_betrothal():
   delta = betrothal1.date() - tod
   return delta.days
 
-def get_words():
+def get_word_data():
   words = requests.get("https://api.shadiao.pro/chp")
   print(words.json()['data']['text'])
   return words.json()['data']['text']
@@ -115,7 +115,7 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"together_days":{"value":get_together()},"birthday_left":{"value":get_birthday()},"birthday_right":{"value":get_birthday_man()},"words":{"value":get_words(), "color":get_random_color()}, "betrothal":{"value":get_betrothal()}, "isWeekDay":{"value":get_weekday()}}
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"together_days":{"value":get_together()},"birthday_left":{"value":get_birthday()},"birthday_right":{"value":get_birthday_man()},"words":{"value":get_word_data(), "color":get_random_color()}, "betrothal":{"value":get_betrothal()}, "isWeekDay":{"value":get_weekday()}}
 res = wm.send_template(user_id, template_id, data)
 wm.send_template(user_id2, template_id, data)
 print(res)
